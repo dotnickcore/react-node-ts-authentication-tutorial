b870f5c618be70a27c62d69fa20b3f56081f5272
trekswithnick <nicholasbrowningdev@outlook.com>
2025-11-01 20:15:21 +1000

    created backend

---
+++ backend/src/index.ts
@@ -0,0 +1,12 @@
+import express from 'express';
+
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.json({ message: 'Hello TypeScript!' });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on http://localhost:${port}`);
+});
